4dd30d930f2932d5b84d04a212e1d90a34d13d91
poby123 <50279318+poby123@users.noreply.github.com>
2023-09-07 21:41:30 +0900

    Initial Commit

---
+++ graphics-start/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  graphics-start
+//
+//  Created by wonju Lee on 2023/09/07.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
